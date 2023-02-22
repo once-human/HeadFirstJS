c7e699c23db5e527621eda0e129e3206d6832d1d
Onkar Yaglewad <20220802065@dypiu.ac.in>
2023-02-22 11:56:19 +0530

    onClick Event Listener in JS - Final Commit

--- Pr_01 - iRock/main.js
+++ Pr_01 - iRock/main.js
@@ -1,1 +1,11 @@
-window.onload="alert('Hello, Im your pet Rock !');"
+function touchRock(){
+    var userName = prompt("What is your Name?", "Enter your name here");
+
+    if (userName){
+        alert("Hey " + userName + ". It good to Meet You !");
+        document.getElementById("irock").src="rock_happy.png";
+    }
+}
+
+document.getElementById("irock").onclick=touchRock();
+onload="alert('Hello, Im your pet Rock !');"
