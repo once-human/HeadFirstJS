7e35745a19ea2e6ba37022703a45359583952274
Onkar Yaglewad <20220802065@dypiu.ac.in>
2023-02-23 23:34:18 +0530

    Master

---
+++ Pr_02 - Duncan Donuts/main.js
@@ -0,0 +1,12 @@
+function updateOrder(){
+    const TAXRATE;
+    const DONUTPRICE;
+
+    var numCakeDonuts = document.getElementById("cakedonuts").value;
+    var numGlazedDonuts = document.getElementById("glazeddonuts").value;
+    var subTotal = (numCakeDonuts + numGlazedDonuts) * DONUTPRICE;
+    var tax = subTotal * TAXRATE;
+    var total = subTotal + tax;
+
+    document.getElementById("subtotal").innerHTML = "$" + subTotal.toFixed(2);
+};
